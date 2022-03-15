4210015e916a88d66f21865bc10220d4fe7c4589
Drozerah <drozerah@gmail.com>
2022-03-15 23:49:09 +0100

    feat: add SASS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ const path = require('path')
 const gulp = require('gulp')
 const { src, dest, series, watch } = require('gulp')
 const gulpEsbuild = require('gulp-esbuild')
+const sass = require('gulp-sass')(require('sass'))
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
 /**
  * Variables 
  */
@@ -23,7 +26,7 @@ const config = {
   },
   scss: {
     src: path.join('.', 'main.scss'),
-    dest: path.join('.', ouputDir, 'css')
+    dest: path.join('.', 'src', 'css')
   },
   js: {
     outfile: 'app.bundle.js',
@@ -59,13 +62,25 @@ gulp.task('esbuild', done => {
   .pipe(dest(config.js.dest))
   return done()
 })
+// Build CSS from SCSS
+gulp.task('build:css', (done) => {
+  src(config.scss.src)
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(cleanCSS())
+    // .pipe(rename('style.css'))
+    .pipe(dest(config.scss.dest))
+  return done()
+})
 /**
  * GULP TASKS SERIES
  */
-gulp.task('build', series('esbuild'))
+gulp.task('build', series('esbuild', 'build:css'))
 /**
  * GULP WATCHERS 
  */
 gulp.task('dev', () => {
+  // watch js
   watch(config.js.src, series('esbuild'))
+  // watch scss
+  watch(config.scss.src, series('build:css'))
 })
