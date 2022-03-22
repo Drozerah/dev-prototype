204dc9a9d55fcb0dce86ad66bbacaf47fd8c1695
Drozerah <drozerah@gmail.com>
2022-03-22 23:23:59 +0100

    chore: refactor dev task

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const path = require('path')
  * Gulp modules 
  */
 const gulp = require('gulp')
-const { src, dest, series, watch } = require('gulp')
+const { src, dest, series, watch, parallel } = require('gulp')
 const gulpEsbuild = require('gulp-esbuild')
 const sass = require('gulp-sass')(require('sass'))
 const cleanCSS = require('gulp-clean-css')
@@ -75,6 +75,7 @@ gulp.task('build:css', (done) => {
   src(config.scss.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(isProduction, cleanCSS())) // minify
+    .pipe(rename('style.css'))
     .pipe(dest(config.scss.dest))
   return done()
 })
@@ -85,9 +86,16 @@ gulp.task('build', series('esbuild', 'build:css'))
 /**
  * GULP WATCHERS 
  */
-gulp.task('dev', () => {
-  // watch js
+function watchJS() {
   watch(config.js.src, series('esbuild'))
-  // watch scss
+}
+function watchCSS() {
   watch(config.scss.src, series('build:css'))
-})
+}
+
+gulp.task('dev', parallel(
+  'esbuild',
+  'build:css',
+  watchJS,
+  watchCSS
+))
